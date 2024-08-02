5e0938f48d93c70edd2e3b7a40e57556e739646c
rohit-gupta001 <81370462+rohitt-gupta@users.noreply.github.com>
2024-08-02 20:32:06 +0530

    Add rust

---
+++ rust/rust-01/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    println!("Hello, world!");
+    // let x: i8 = 10;
+    // let y: i8 = 50;
+    // let z: i8 = 126;
+
+    // println!("x = {}, y = {}, z = {}", x, y, z);
+
+    let is_male: bool = true;
+
+    if is_male {
+        println!("You are a male!")
+    } else {
+        println!("you are not a male")
+    }
+}
